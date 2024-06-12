97f12acb1d2bab790ebd3c4c80fb827f6b25348f
Andrew Korell <AndrewKorell@gmail.com>
2024-06-12 17:36:26 -0400

    Add .mk file gitignore

--- Debug/Core/Startup/subdir.mk
+++ Debug/Core/Startup/subdir.mk
@@ -7,16 +7,16 @@
 S_SRCS += \
 ../Core/Startup/startup_stm32f446zetx.s 
 
-OBJS += \
-./Core/Startup/startup_stm32f446zetx.o 
-
 S_DEPS += \
 ./Core/Startup/startup_stm32f446zetx.d 
+
+OBJS += \
+./Core/Startup/startup_stm32f446zetx.o 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/git/STMProjects/Nucleo_446ZE/Personal_DAC_01/Common" -I"C:/git/STMProjects/Nucleo_446ZE/Personal_DAC_01/Testing" -I"C:/git/STMProjects/Nucleo_446ZE/Personal_DAC_01/Common/inc" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
